574cd83ec80503d00ac855d8b860a18fa4c618ce
labswingsquare <labs.wingsquare@gmail.com>
2021-12-02 10:49:22 +0530

    Committed with new user labswingsquare

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Node App, power the web with Docker & Docker Compose !!!
+        Node App, power the web with Docker & Docker Compose !!! by labswingsquare user
         </p>
 	  <p>
 	  Learn React
